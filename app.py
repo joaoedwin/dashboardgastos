ec850d5d16f65ee2211b954f9deb337273d9efe8
joaoedwin <joaoedumiranda@gmailcom>
2024-05-15 17:12:34 -0300

    Corrigindo main.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import os
 from main import app as application
 
 if __name__ == '__main__':
-    application.run_server(host='0.0.0.0', port=8050)
+    port = int(os.environ.get('PORT', 8050))  # Verifica se a variável de ambiente PORT está definida
+    application.run_server(host='0.0.0.0', port=port, debug=True)
